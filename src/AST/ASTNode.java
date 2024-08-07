b51db21d91a3d5c7ba0a451386651854bdca321c
Barry <2411557145@qq.com>
2024-08-08 01:58:48 +0800

    开始构建AST

--- src/AST/ASTNode.java
+++ src/AST/ASTNode.java
@@ -1,4 +1,12 @@
 package AST;
 
-public class ASTNode {
+import org.antlr.runtime.tree.TreeWizard;
+
+abstract public class ASTNode {
+    public position pos;
+    public ASTnode(position pos){
+        this.pos = pos;
+    }
+
+    abstract public void accept(ASTVisitor visitor);
 }
---
+++ src/AST/Stmt/StmtNode.java
@@ -0,0 +1,10 @@
+package AST.Stmt;
+
+import AST.ASTNode;
+
+public abstract class StmtNode extends ASTNode {
+    public StmtNode(position pos) {
+        super(pos);
+    }
+
+}
